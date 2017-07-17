8a426ff1bb4ff807ea7bef0f7578513fc64d8f9b
3225297@gmail.com <3225297@gmail.com>
2017-07-17 19:14:09 +0800

    static

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/static'));
 
 app.listen(process.env.PORT || 8080);
