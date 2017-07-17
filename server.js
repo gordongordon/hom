5c850862f2f39a0ecda891e215fa0af5c4d56cea
3225297@gmail.com <3225297@gmail.com>
2017-07-17 19:41:34 +0800

    dd

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
-var express = require('express');
-var app = express();
+var webpack = require('webpack');
+var WebpackDevServer = require('webpack-dev-server');
+var config = require('./webpack.config');
 
-app.use(express.static(__dirname + '/static'));
+new WebpackDevServer(webpack(config), {
+  publicPath: config.output.publicPath,
+  hot: true,
+  historyApiFallback: true,
+  host : '0.0.0.0'
+}).listen(3000, 'localhost', function (err, result) {
+  if (err) {
+    console.log(err);
+  }
 
-app.listen(process.env.PORT || 8080);
+  console.log('Listening at localhost:3000');
+});
